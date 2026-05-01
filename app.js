45f32802b8f43c1cbe2d8ec912e1350a5081f831
somabeleh <s12429370@stu.najah.edu>
2026-05-02 01:37:01 +0300

    Adding controllers,routes

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const nutritionRoutes = require('./routes/nutritionRoutes');
 const userRoutes = require('./routes/userRoutes');
-
+const membershipRoutes = require('./routes/membershipRoutes');
+const subscriptionRoutes = require('./routes/subscriptionRoutes');
 const app = express();
 
 app.use(cors());
@@ -11,6 +12,8 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use('/api/nutrition', nutritionRoutes);
 app.use('/users', userRoutes);
+app.use('/api/memberships', membershipRoutes);
+app.use('/api/subscriptions', subscriptionRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
