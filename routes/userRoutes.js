b0477a703a0332445771959eb92855b4b8453030
RoaaHashesh123 <s12429422@stu.najah.edu>
2026-05-15 18:31:06 +0300

    Add user retrieval and deletion routes with middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,15 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/userController');
+
 const authMiddleware = require('../middleware/authMiddleware');
+const roleMiddleware = require('../middleware/roleMiddleware');
 
 router.post('/login', userController.loginUser);
+
 router.post('/', userController.createUser);
 
 router.get('/:id', authMiddleware, userController.getUserById);
-router.delete('/:id', authMiddleware, userController.deleteUser);
 
-module.exports = router;    
+router.get('/', authMiddleware, roleMiddleware('admin'), userController.getAllUsers);
 
-                                                                                                                                  
+router.delete('/:id', authMiddleware, roleMiddleware('admin'), userController.deleteUser);
 
+module.exports = router;
