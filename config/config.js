9f89a97d0f92fb3f17fb64d295431d2fa066903b
Obayda-Amer <s12429584@stu.najah.edu>
2026-05-16 21:13:33 +0300

    debug env

--- config/config.js
+++ config/config.js
@@ -4,13 +4,13 @@ console.log("DB_HOST =", process.env.DB_HOST);
 console.log("DB_NAME =", process.env.DB_NAME);
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || 'obayda',
-    database: process.env.DB_NAME || 'fitness_time',
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 3306,
-    dialect: 'mysql',
-  },
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
+},
 
   test: {
     username: process.env.DB_USERNAME || 'root',
