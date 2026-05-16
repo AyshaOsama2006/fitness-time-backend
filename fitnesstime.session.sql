8c0278358937697e80274131460e93957cc7d5b3
yaqeenashour <ashouryaqen@gmail.com>
2026-05-16 23:07:52 +0300

    save local config changes

---
+++ fitnesstime.session.sql
@@ -0,0 +1,20 @@
+INSERT INTO memberships (
+    id,
+    M_id,
+    name,
+    Price,
+    Description,
+    DurationMonths,
+    createdAt,
+    updatedAt
+  )
+VALUES (
+    id:int,
+    M_id:int,
+    'name:varchar',
+    'Price:float',
+    'Description:varchar',
+    DurationMonths:int,
+    'createdAt:datetime',
+    'updatedAt:datetime'
+  );
