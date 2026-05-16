18c01de4f99afebf4b7960919acc33e9f2bc7b3a
Obayda-Amer <s12429584@stu.najah.edu>
2026-05-16 21:17:47 +0300

    force railway db config

--- config/config.js
+++ config/config.js
@@ -1,23 +1,21 @@
 require('dotenv').config();
-console.log("NODE_ENV =", process.env.NODE_ENV);
-console.log("DB_HOST =", process.env.DB_HOST);
-console.log("DB_NAME =", process.env.DB_NAME);
+
 module.exports = {
   development: {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'mysql',
-},
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+  },
 
   test: {
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || 'obayda',
-    database: process.env.DB_NAME || 'fitness_time',
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 3306,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
   },
 
