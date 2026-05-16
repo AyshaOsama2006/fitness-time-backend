8139646fed1a0ad500441ffe3f0751ee356461f7
Obayda-Amer <s12429584@stu.najah.edu>
2026-05-16 21:09:05 +0300

    fix db env names

--- config/config.js
+++ config/config.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-  username: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || 'obayda',
-  database: process.env.MYSQLDATABASE || 'fitness_time',
-  host: process.env.MYSQLHOST || '127.0.0.1',
-  port: process.env.MYSQLPORT || 3306,
-  dialect: 'mysql',
-},
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || 'obayda',
+    database: process.env.DB_NAME || 'fitness_time',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
+  },
 
   test: {
     username: process.env.DB_USERNAME || 'root',
@@ -16,15 +16,15 @@ module.exports = {
     database: process.env.DB_NAME || 'fitness_time',
     host: process.env.DB_HOST || '127.0.0.1',
     port: process.env.DB_PORT || 3306,
-    dialect: process.env.DB_DIALECT || 'mysql',
+    dialect: 'mysql',
   },
 
   production: {
-  username: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-  dialect: 'mysql',
-},
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+  },
 };
